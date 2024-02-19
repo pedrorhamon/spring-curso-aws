d8f781479da5e47134dceb2ac814a1d8e6653c91
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-19 20:00:42 -0300

    primeira entity do sistema

--- src/main/java/com/starking/dsleam/entities/Role.java
+++ src/main/java/com/starking/dsleam/entities/Role.java
@@ -1,14 +1,27 @@
 package com.starking.dsleam.entities;
 
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @author pedroRhamon
  */
 
 @Getter
 @Setter
-@All
-public class Role {
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Role implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
+
+	@EqualsAndHashCode.Include
 	private Long id;
 	
 	private String authority;
