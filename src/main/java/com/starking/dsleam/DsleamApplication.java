284cf497df576d6ebf798fc02f0427450ed73dcf
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-19 19:50:12 -0300

    Criando projeto

---
+++ src/main/java/com/starking/dsleam/DsleamApplication.java
@@ -0,0 +1,13 @@
+package com.starking.dsleam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsleamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsleamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starking/dsleam/DsleamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starking.dsleam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsleamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
