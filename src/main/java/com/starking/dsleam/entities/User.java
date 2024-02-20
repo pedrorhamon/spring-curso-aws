f5892e920a9086667555c33d57d79407b5ff6c4e
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-20 19:54:20 -0300

    mapeando o User e Role

--- src/main/java/com/starking/dsleam/entities/User.java
+++ src/main/java/com/starking/dsleam/entities/User.java
@@ -5,9 +5,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -41,6 +45,9 @@ public class User implements Serializable{
 	
 	private String password;
 	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 
 }
