254fe925a554210d7b116f155386e9675c54440f
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-21 19:13:14 -0300

    User e Role

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 INSERT INTO tb_user (name, email, password) values ('Pedro', 'pedro@gmail.com', '123');
-INSERT INTO tb_user (name, email, password) values ('Pedro', 'pedro@gmail.com', '123');
+INSERT INTO tb_user (name, email, password) values ('Rhamon', 'rhamon@gmail.com', '123');
+INSERT INTO tb_user (name, email, password) values ('Sousa', 'sousa@gmail.com', '123');
 
 INSERT INTO tb_role (authority) values ('ROLE_STUDENT');
 INSERT INTO tb_role (authority) values ('ROLE_INSTRUCTOR');
