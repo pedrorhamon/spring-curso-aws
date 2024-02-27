ae3d16040ba47f82fe5972ee7e3329d39c3aff66
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-26 22:22:34 -0300

    adicionando o atributo na entidadade

--- src/main/java/com/starking/dsleam/entities/Section.java
+++ src/main/java/com/starking/dsleam/entities/Section.java
@@ -1,8 +1,19 @@
 package com.starking.dsleam.entities;
 
+import java.io.Serializable;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 /**
  * @author pedroRhamon
  */
-public class Section {
+public class Section implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 }
