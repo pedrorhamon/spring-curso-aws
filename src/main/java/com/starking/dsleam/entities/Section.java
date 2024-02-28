0b030acf9876433818719b959c8196107774513a
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-28 19:34:48 -0300

    adicionando atributos e anotacoes

--- src/main/java/com/starking/dsleam/entities/Section.java
+++ src/main/java/com/starking/dsleam/entities/Section.java
@@ -2,18 +2,42 @@ package com.starking.dsleam.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * @author pedroRhamon
  */
+
+@Entity
+@Table(name = "tb_section")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Section implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
 	private Long id;
+	
+	private String title;
+	
+	private String description;
+	
+	private Integer position;
+	
+	private String imgUri;
 }
