744bc4e0b24eb67c460da7015b2ab7f1172d1370
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-29 22:29:34 -0300

    criando description no enum

--- src/main/java/com/starking/dsleam/entities/enums/ResourceType.java
+++ src/main/java/com/starking/dsleam/entities/enums/ResourceType.java
@@ -1,13 +1,21 @@
 package com.starking.dsleam.entities.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * @author pedroRhamon
  */
+
+@AllArgsConstructor
+@Getter
 public enum ResourceType {
 	
-	LESSON_ONLY,
-	LESSON_TASK,
-	FORUM,
-	EXTERNAL_LINK
+	LESSON_ONLY("Lesson Only"),
+	LESSON_TASK("Lesson task"),
+	FORUM("Forum"),
+	EXTERNAL_LINK("External link");
+	
+	private String description;
 
 }
