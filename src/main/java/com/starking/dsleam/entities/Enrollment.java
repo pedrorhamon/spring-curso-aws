6e0207b4327604e23d70ef5aa863b06fc62de02f
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-03-02 20:54:58 -0300

    criando os atributos de Enrollment

--- src/main/java/com/starking/dsleam/entities/Enrollment.java
+++ src/main/java/com/starking/dsleam/entities/Enrollment.java
@@ -1,12 +1,33 @@
 package com.starking.dsleam.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * @author pedroRhamon
  */
-public class Enrollment implements Serializable{
+
+@Entity
+@Table(name = "tb_resource")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Enrollment implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	private Instant enrollMoment;
+	private Instant refundMoment;
+	private boolean available = true;
+	private boolean onlyUpdate;
+	
 
 }
