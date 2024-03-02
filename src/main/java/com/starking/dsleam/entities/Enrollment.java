67744039b1e88d1bcec157b57dccaa4d0c75b575
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-03-02 20:44:17 -0300

    criando puro a entidade

---
+++ src/main/java/com/starking/dsleam/entities/Enrollment.java
@@ -0,0 +1,12 @@
+package com.starking.dsleam.entities;
+
+import java.io.Serializable;
+
+/**
+ * @author pedroRhamon
+ */
+public class Enrollment implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+}
