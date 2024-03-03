9b85a212316ebadd9acd4e663665cbcd9971b2d7
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-03-02 21:57:19 -0300

    implementando Lesson e seus atributos

--- src/main/java/com/starking/dsleam/entities/Lesson.java
+++ src/main/java/com/starking/dsleam/entities/Lesson.java
@@ -1,8 +1,44 @@
 package com.starking.dsleam.entities;
 
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @author pedroRhamon
  */
-public class Lesson {
+@Entity
+@Table(name = "tb_section")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Lesson implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	private String title;
+	
+	private Integer position;
+	
+	private Section section;
+	
+	private Set<Enrollment> enrollments = new HashSet<>();
 }
